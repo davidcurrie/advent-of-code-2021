e6b99d6c3d48cf16b1fc5645ad2dbcc5a392a22e
David Currie <dcurrie@cloudbees.com>
2021-12-13 09:38:14 +0000

    Day 13

---
+++ src/day13/result.kt
@@ -0,0 +1,35 @@
+package day13
+
+import java.io.File
+import kotlin.math.abs
+
+fun main() {
+    val input = File("src/day13/input.txt").readText()
+
+    val coords = Regex("([0-9]*),([0-9]*)").findAll(input)
+        .map { Pair(it.groupValues[1].toInt(), it.groupValues[2].toInt()) }
+        .toSet()
+    val folds = Regex("fold along ([xy])=([0-9]*)").findAll(input)
+        .map { Pair(it.groupValues[1], it.groupValues[2].toInt()) }
+        .toList()
+
+    println(coords.map { p -> p.apply(folds[0]) }.toSet().size)
+    folds.fold(coords) { c, f -> c.map { p -> p.apply(f) }.toSet() }.output()
+}
+
+fun Pair<Int, Int>.apply(fold: Pair<String, Int>): Pair<Int, Int> {
+    return Pair(
+        if (fold.first == "x") fold.second - abs(first - fold.second) else first,
+        if (fold.first == "y") fold.second - abs(second - fold.second) else second
+    )
+}
+
+fun Set<Pair<Int, Int>>.output() {
+    (0..maxOf { it.second }).forEach { y ->
+        (0..maxOf { it.first }).forEach {
+                x -> print(if (contains(Pair(x, y))) "#" else ".")
+        }
+        println()
+    }
+    println()
+}
