5488c17e8f7394bb0b35c4a9c1d52e7762a0d023
David Currie <dcurrie@cloudbees.com>
2021-12-20 12:35:20 +0000

    Fix size

--- src/day20/result.kt
+++ src/day20/result.kt
@@ -4,20 +4,19 @@ import java.io.File
 
 fun Array<Array<Boolean>>.count() = sumOf { row -> row.count { it } }
 
-fun Array<Array<Boolean>>.enhance(algo: List<Boolean>, infinity: Boolean) : Array<Array<Boolean>> {
-    return (-1..size).map { y ->
+fun Array<Array<Boolean>>.enhance(algo: List<Boolean>, infinity: Boolean) =
+    (-1..size).map { y ->
         (-1..this[0].size).map { x ->
             algo[index(x, y, infinity)]
         }.toTypedArray()
     }.toTypedArray()
-}
 
-fun Array<Array<Boolean>>.index(x : Int, y : Int, infinity: Boolean) =
+fun Array<Array<Boolean>>.index(x: Int, y: Int, infinity: Boolean) =
     (-1..1).map { dy -> (-1..1).map { dx -> if (get(x + dx, y + dy, infinity)) 1 else 0 } }
         .flatten().joinToString("").toInt(2)
 
-fun Array<Array<Boolean>>.get(x : Int, y : Int, infinity: Boolean) =
-    if (y < 0 || y >= size || x < 0 || x >= size ) infinity else this[y][x]
+fun Array<Array<Boolean>>.get(x: Int, y: Int, infinity: Boolean) =
+    if (y < 0 || y >= size || x < 0 || x >= this[0].size) infinity else this[y][x]
 
 fun main() {
     val lines = File("src/day20/input.txt").readLines()
